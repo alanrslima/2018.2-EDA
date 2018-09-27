014295fa3e387980a52e5185764e99c33966cbd2
Alan Lima <alanronison.lima@gmail.com>
2018-09-27 12:40:23 -0300

    Update na funcao de get asphalt image

--- projetos/02/Projeto2.c
+++ projetos/02/Projeto2.c
@@ -3,65 +3,70 @@
 #include <stdbool.h>
 #include <time.h>
 
-void GeraAleatorios(int numeros[], int quantNumeros, int Limite);
-bool Existe(int valores[], int tam, int valor);
-FILE* get_asphalt_images(int random);
+void GeraAleatorios(int *, int tamVet, int limite);
+bool Existe(int *, int tamVet, int valor);
+void getAsphaltImage(FILE *,int random);
 
 int main(){
-	FILE *asphalt_images[25], *grass_images[25];
-	int aleatorios[25];
+  FILE *asphalt_images[25], *grass_images[25];
+  int grass[25], asphalt[25];
 
-	GeraAleatorios(aleatorios, 25, 50);
+  GeraAleatorios(asphalt, 25, 50);
 
-	for(int i=0; i<25; i++)
-    get_asphalt_images(aleatorios[i]);
+  printf("Ordem do vetor de asfalto: \n");
+  for(int i=0; i<25; i++){
+    printf("%d ", asphalt[i]);
+  }
+  printf("\n-----------------------\n");
 
-	return 0;
+  // for(int i=0; i<25; i++){
+    // print("Arquivo número %d", i);
+
+      FILE *fileAsphalt;
+      getAsphaltImage(fileAsphalt, asphalt[0]);
+
+  // }
+
+
+  return 0;
 }
 
-FILE* get_asphalt_images(int random){
+void getAsphaltImage(FILE *fp ,int id){
 
-	FILE *fp;
-	char asphalt[25];
+  char asphalt[25];
 
-	if(random < 11)
-		sprintf(asphalt, "/home/marco/Documentos/2018.2-EDA/projetos/02/asphalt/asphalt_0%d.txt",random);
-	else
-		sprintf(asphalt, "/home/marco/Documentos/2018.2-EDA/projetos/02/asphalt/asphalt_%d.txt",random);
+  if(id < 10)
+    sprintf(asphalt, "asphalt/asphalt_0%d.txt",id);
+  else
+    sprintf(asphalt, "asphalt/asphalt_%d.txt",id);
 
-	printf("Arquivo: %s\n", asphalt);
+  printf("Arquivo: %s\n", asphalt);
+  fp = fopen(asphalt ,"r");
 
-	fp = fopen(asphalt ,"r");
-
-	if(fp==NULL){
-		printf("Falha.\n");
-		exit(1);
-	}
-
-	return fp;
+  if(fp==NULL){
+    printf("Falha.\n");
+    exit(1);
+  }
 }
 
-bool Existe(int valores[], int tam, int valor)
+bool Existe(int *vet, int tamVet, int valor)
 {
-  for(int i=0; i<tam; i++){
-    if(valores[i]==valor)
+  for(int i=0; i<tamVet; i++){
+    if(vet[i]==valor || vet[i]==0)
       return true;
   }
-
   return false;
 }
 
-void GeraAleatorios(int aleatorios[], int quantNumeros, int Limite)
+void GeraAleatorios(int *vet, int tamVet, int limite)
 {
   srand(time(NULL));
-
-  int v;
-  for(int i=0; i<quantNumeros; i++){
-    v = rand() % Limite;
-    while (Existe(aleatorios, i, v)) {
-      v = rand() % Limite;
+  int random;
+  for(int i=0; i<tamVet; i++){
+    random = rand() % limite;
+    while (Existe(vet, i, random)) {
+      random = rand() % limite;
     }
-    aleatorios[i] = v;
+    vet[i] = random;
   }
-
 }
