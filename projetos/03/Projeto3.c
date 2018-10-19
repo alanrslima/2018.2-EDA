03a7f04ace969e53720e4d118a5dfb6f6ea3c38d
alanrslima <alanronison.lima@gmail.com>
2018-10-19 01:36:49 -0300

    Criacao de struct de contatos

--- projetos/03/Projeto3.c
+++ projetos/03/Projeto3.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct contato {
+    char nome[101];
+    char celular[11];
+    char endereco[101];
+    unsigned int cep;
+    char data_nascimento[11];
+    struct contato *prox;
+    struct contato *ant;
+}
+
+typedef struct contato Contato;
+
 int main(int argc, char const *argv[]) {
 
   return 0;
