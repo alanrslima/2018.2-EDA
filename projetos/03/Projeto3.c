d63394ba994905250236976a86b0c8d167d62e62
alanrslima <alanronison.lima@gmail.com>
2018-10-21 12:23:03 -0300

    [3] Criado acesso ao menu e validação de entrada do usuario

--- projetos/03/Projeto3.c
+++ projetos/03/Projeto3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <curses.h>
 
 struct contato {
   char nome[101];
@@ -18,6 +19,7 @@ Contato *abreArquivo();
 Contato *insereInicioLista(Contato *lista, Contato *contato);
 void imprimirContatos(Contato *lista);
 void criaMenu();
+void acessaMenu(Contato *lista);
 
 
 int main(int argc, char const *argv[]) {
@@ -26,8 +28,8 @@ int main(int argc, char const *argv[]) {
 
   listaContatos = criaListaVazia();
   listaContatos = abreArquivo(listaContatos);
-  imprimirContatos(listaContatos);
   criaMenu();
+  acessaMenu(listaContatos);
 
   return 0;
 }
@@ -83,12 +85,13 @@ void imprimirContatos(Contato *lista) {
     printf("Lista de contatos vazia!\n");
   }
 
+  printf("\n Lista de contatos: \n");
   while (index != NULL) {
-    printf("Nome: %s\n", index->nome);
+    printf("\nNome: %s\n", index->nome);
     printf("Telefone: %s\n", index->celular);
     printf("Endereço: %s\n", index->endereco);
     printf("CEP: %d\n", index->cep);
-    printf("Aniversário: %s\n\n", index->dataNascimento);
+    printf("Aniversário: %s\n", index->dataNascimento);
     index = index->proximo;
   }
 }
@@ -102,7 +105,7 @@ Contato *insereInicioLista(Contato *lista, Contato *contato) {
 }
 
 void criaMenu(){
-  printf("----------------------------------------------------------\n");
+  printf("\n\n\n----------------------------------------------------------\n");
   printf(">>>>>>>>>>>>>>>>>>> LISTA DE CONTATOS <<<<<<<<<<<<<<<<<<<<\n");
   printf("----------------------------------------------------------\n\n");
   printf("\t Menu de opções, selecione a desejeda: \n\n");
@@ -112,4 +115,36 @@ void criaMenu(){
   printf("  (4) Visualizar todos os contatos em ordem alfabetica \n");
   printf("  (5) Sair \n\n");
   printf("----------------------------------------------------------\n");
+  printf("Digite aqui: ");
+}
+
+void acessaMenu(Contato *lista){
+  char itemSelecionado = '0';
+
+  while (itemSelecionado != '5'){
+    scanf(" %c", &itemSelecionado);
+    switch (itemSelecionado) {
+      case '1':
+        printf("\n\nInserir novo contato\n");
+        criaMenu();
+        break;
+      case '2':
+        printf("\n\nRemover contato com base no nome\n");
+        criaMenu();
+        break;
+      case '3':
+        printf("\n\nVisualizar contato com base no nome\n");
+        criaMenu();
+        break;
+      case '4':
+        imprimirContatos(lista);
+        criaMenu();
+        break;
+      case '5':
+        printf("\n\nSair do programa \n (Escrever arquivo e limpar lista da memoria)\n\n");
+        break;
+      default:
+        printf("Entrada inválida! Digite novamente: ");
+    }
+  }
 }
