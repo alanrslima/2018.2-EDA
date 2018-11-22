99e4882cbdda94d7928909ae4ff4a68ade492464
Alan Lima <alanronison.lima@gmail.com>
2018-11-22 13:54:00 -0200

    Adicionado leitura de parametro por linha de comando

--- projetos/06/Projeto6.c
+++ projetos/06/Projeto6.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[]) {
+int get_parametro_linha_comando(int argc, char *argv[], int *param);
 
+int main(int argc, char *argv[]) {
+  int param;
+
+  if (get_parametro_linha_comando(argc, argv, &param)){
+    printf("Parametro de entrada = %d\n", param );
+  }
   return 0;
+}
+
+int get_parametro_linha_comando(int argc, char *argv[], int *param){
+  if (argc == 1){
+    printf("Programa %s sem parametros\n", argv[0] );
+    return 0;
+  }else{
+    *param = atoi(argv[1]);
+    return 1;
+  }
 }
